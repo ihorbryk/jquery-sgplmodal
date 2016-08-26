e74d151c49c7da8f77007cacd8e6418219f537d2
ihorbryk <ihorbryk@gmail.com>
2016-08-26 11:46:59 +0300

    Small fix

--- js/jquery.sgplmodal.js
+++ js/jquery.sgplmodal.js
@@ -2,13 +2,22 @@
     $(document).ready(function($) {
         $.sgplmodal = {
 
-            maxWidth : 400,
+            selector: '',
+            maxWidth : 330,
             beforeOpenSubscriber : [],
+            afterOpenSubscriber : [],
             beforeCloseSubscriber : [],
             contentSelector: '',
+            content: '',
 
             beforeOpenTriger : function() {
                 this.beforeOpenSubscriber.map(function(func) {
+                    return func();
+                });
+            },
+
+            afterOpenTriger : function() {
+                this.afterOpenSubscriber.map(function(func) {
                     return func();
                 });
             },
@@ -21,6 +30,10 @@
 
             beforeOpen : function(func) {
                 this.beforeOpenSubscriber.push(func);
+            },
+
+            afterOpen : function(func) {
+                this.afterOpenSubscriber.push(func);
             },
 
             beforeClose : function(func) {
@@ -38,25 +51,38 @@
             },
 
             open : function(selector) {
+                console.log('try open');
+                this.selector = selector;
+
                 this.beforeOpenTriger();
 
                 var topIndent = this.getTopIndent();
 
-                var content = $(selector).html();
+                this.content = $(selector).html();
+                $(selector).empty();
+
                 $('body').append('<div class="overlay"></div>');
                 $('body').append('<div id="modal-content-wrapper" style="top: ' + topIndent + 'px; max-width: ' + this.maxWidth + 'px;"><div id="modal-content"><div class="modal-close">X</div></div></div>');
 
                 $('body').css('overflow-x', 'hidden');
-                $('#modal-content').append(content);
+                $('#modal-content').append(this.content);
+
+                this.afterOpenTriger();
             },
 
             close : function() {
-                this.beforeClose();
+                console.log('try close');
+                this.beforeCloseTriger();
 
                 $('.overlay').remove();
                 $('#modal-content-wrapper').remove();
                 $('body').css('overflow-x', 'none');
                
+                this.beforeOpenSubscriber = [];
+                this.afterOpenSubscriber = [];
+                this.beforeCloseSubscriber = [];
+
+                $(this.selector).html(this.content);
             },
 
             bindClose : $('body').on('click', 'div.modal-close', function(e) {
